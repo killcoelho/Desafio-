2898687925c92793449080f479ec7cf0521baedf
killcoelho <124314279+killcoelho@users.noreply.github.com>
2023-04-05 21:15:39 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,143 @@
+
+var n1 = parseInt (34);
+var n2 = parseFloat (4.9);
+
+var soma = n1+n2;
+ console.log (soma);
+
+ var n1 = parseInt (56);
+var n2 = parseFloat (45.9);
+
+var soma = n1+n2;
+ console.log (soma);
+
+ var n1 = parseInt (56);
+var n2 = parseFloat (334.9);
+
+var soma = n1+n2;
+ console.log (soma);
+
+
+ var n1 = parseInt (56);
+var n2 = parseFloat (49.6);
+
+var soma = n1+n2;
+ console.log (soma);
+
+
+ var n1 = parseInt (12);
+var n2 = parseFloat (45.6);
+
+var soma = n1+n2;
+ console.log (soma);
+
+
+ var n1 = parseInt (54);
+var n2 = parseFloat (4.98);
+
+var soma = n1*n2;
+ console.log (soma);
+
+
+ var n1 = parseInt (354);
+var n2 = parseFloat (56.9);
+
+var soma = n1*n2;
+ console.log (soma);
+
+
+ var n1 = parseInt (37);
+var n2 = parseFloat (6.8);
+
+var soma = n1*n2;
+ console.log (soma);
+
+
+ var n1 = parseInt (344);
+var n2 = parseFloat (98.9);
+
+var soma = n1*n2;
+ console.log (soma);
+
+
+ var n1 = parseInt (3444);
+var n2 = parseFloat (4.7);
+
+var soma = n1*n2;
+ console.log (soma);
+
+
+ var n1 = parseInt (384);
+var n2 = parseFloat (87.9);
+
+var soma = n1-n2;
+ console.log (soma);
+
+
+ var n1 = parseInt (3499);
+var n2 = parseFloat (4.65);
+
+var soma = n1-n2;
+ console.log (soma);
+
+
+ var n1 = parseInt (544);
+var n2 = parseFloat (9.77);
+
+var soma = n1-n2;
+ console.log (soma);
+
+ var n1 = parseInt (377);
+ var n2 = parseFloat (33.7);
+ 
+ var soma = n1-n2;
+  console.log (soma);
+
+
+  var n1 = parseInt (77);
+var n2 = parseFloat (5.7);
+
+var soma = n1-n2;
+ console.log (soma);
+
+ var n1 = parseInt (3497);
+ var n2 = parseFloat (8.7);
+ 
+ var soma = n1/n2;
+  console.log (soma);
+
+  var n1 = parseInt (654);
+  var n2 = parseFloat (4.6);
+  
+  var soma = n1/n2;
+   console.log (soma);
+
+   var n1 = parseInt (6477);
+   var n2 = parseFloat (4.3);
+   
+   var soma = n1/n2;
+    console.log (soma);
+
+    var n1 = parseInt (237);
+    var n2 = parseFloat (22.7);
+    
+    var soma = n1/n2;
+     console.log (soma);
+
+     var n1 = parseInt (997);
+     var n2 = parseFloat (4.54);
+     
+     var soma = n1/n2;
+      console.log (soma);
+
+      API É CONJUNTO DE ROTINAS E PADRÕES  DEFINIDOS POR UM SOFTWARE PARA A DEFINIÇAO 
+      DE SUAS FUNCIONALIDADES POR OUTROS APLICATIVOS
+
+
+
+ 
+
+
+
+
+
---
+++ programa.js
@@ -0,0 +1,51 @@
+var frase="aprendendo Java script"
+console.log(frase)
+var numero = 1100;
+var zero = 0;
+var numeroNegativo = -250;
+var numeroReal = 25.65;
+var pi = 3.1415;
+var realNegativo = -19.13;
+var realZero = 10.0;
+
+
+function returnGame(){
+    var game = 'God of War';
+    if(game === 'God of War'){
+      var game = 'Valorant';
+console.log('Dentro do if:' + game);
+}
+console.log('Fora do if:'+ game);
+}
+returnGame()
+
+function returnGame(){
+    let game = 'God of War';
+    if(game === 'God of War'){
+      let game = 'Valorant';
+console.log('Dentro do if:' + game);
+}
+console.log('Fora do if:'+ game);
+}
+returnGame()
+
+const USER_ADMIN = {name:'Lucas Freitas',
+login:'root'
+};
+USER_ADMIN.login='admin'; // Alteramos o valor da propriedade login
+console.log(USER_ADMIN);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
